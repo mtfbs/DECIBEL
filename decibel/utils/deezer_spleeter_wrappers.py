6f0e0036283a17dc28130a5979efd8c2e8491d81
Mateus Ferreira <mateusfbsoares@gmail.com>
2021-06-04 16:56:52 -0300

    fix: fix minor Deezer spleeter bugs.

--- decibel/utils/deezer_spleeter_wrappers.py
+++ decibel/utils/deezer_spleeter_wrappers.py
@@ -6,14 +6,17 @@ import shutil
 from pathlib import Path
 
 
-def spleet_song(file_path="song.mp3", output_file_path="spleeted_song.mp3", number_of_stems=2):
+def spleet_song(working_directory_absolute_path=None, input_file_name="song.mp3", output_file_name="spleeted_song.mp3",
+                number_of_stems=2):
     """
     The output is:       only one file without vocals        --> 2 stems   or
                          only one file without vocals and bass --> 4 stems
     """
 
+    file_absolute_path = working_directory_absolute_path + '/' + input_file_name
+
     # open file with pydub
-    song = AudioSegment.from_mp3(file_path)
+    song = AudioSegment.from_mp3(file_absolute_path)
     song_channels = song.split_to_mono()
     song = song_channels[0]
 
@@ -37,12 +40,13 @@ def spleet_song(file_path="song.mp3", output_file_path="spleeted_song.mp3", numb
     # write song slices to memory
     number_of_slices = len(song_slices)
     for i in range(0, number_of_slices):
-        song_slices[i].export(f"song_slice_{i}.mp3")
+        song_slices[i].export(f"{working_directory_absolute_path}/song_slice_{i}.mp3")
 
     # call deezer spleeter on each slice
     print("Calling deezer spleeter")
     for i in range(0, number_of_slices):
-        os.system(f'spleeter separate -p spleeter:{number_of_stems}stems -o output song_slice_{i}.mp3')
+        os.system(
+            f'spleeter separate -p spleeter:{number_of_stems}stems -o {working_directory_absolute_path}/output {working_directory_absolute_path}/song_slice_{i}.mp3')
 
     # concatenate slices and write final file
     print("Concatenating spleeted files")
@@ -50,26 +54,29 @@ def spleet_song(file_path="song.mp3", output_file_path="spleeted_song.mp3", numb
     # 2 stems
     if number_of_stems == 2:
         for i in range(0, number_of_slices):
-            spleeted_song_slices.append(AudioSegment.from_mp3(f"output/song_slice_{i}/accompaniment.wav"))
+            spleeted_song_slices.append(
+                AudioSegment.from_mp3(f"{working_directory_absolute_path}/output/song_slice_{i}/accompaniment.wav"))
         song_without_vocals = spleeted_song_slices[0]
         if number_of_slices > 0:
             for i in range(1, number_of_slices):
                 song_without_vocals += spleeted_song_slices[i]
         # writes
         song = AudioSegment.from_mono_audiosegments(song, song)
-        song_without_vocals.export(output_file_path)
+        song_without_vocals.export(f"{working_directory_absolute_path}/{output_file_name}")
         # delete residual files
-        shutil.rmtree('output')
+        shutil.rmtree(f'{working_directory_absolute_path}/output')
         for i in range(0, number_of_slices):
-            os.remove(f"song_slice_{i}.mp3")
+            os.remove(f"{working_directory_absolute_path}/song_slice_{i}.mp3")
         print("successs.")
     # 4 stems
     elif number_of_stems == 4:
         spleeted_song_slices_drums = []
         spleeted_song_slices_other = []
         for i in range(0, number_of_slices):
-            spleeted_song_slices_drums.append(AudioSegment.from_mp3(f"output/song_slice_{i}/drums.wav"))
-            spleeted_song_slices_other.append(AudioSegment.from_mp3(f"output/song_slice_{i}/other.wav"))
+            spleeted_song_slices_drums.append(
+                AudioSegment.from_mp3(f"{working_directory_absolute_path}/output/song_slice_{i}/drums.wav"))
+            spleeted_song_slices_other.append(
+                AudioSegment.from_mp3(f"{working_directory_absolute_path}/output/song_slice_{i}/other.wav"))
         song_with_only_drums = spleeted_song_slices_drums[0]
         song_with_only_other = spleeted_song_slices_other[0]
         if number_of_slices > 0:
@@ -79,17 +86,32 @@ def spleet_song(file_path="song.mp3", output_file_path="spleeted_song.mp3", numb
         # two channels to one
         song_without_drums_and_bass = song_with_only_drums.overlay(song_with_only_other)
         # writes
-        song_without_drums_and_bass.export(output_file_path)
+        song_without_drums_and_bass.export(f"{working_directory_absolute_path}/{output_file_name}")
         # delete residual files
-        shutil.rmtree('output')
+        shutil.rmtree(f'{working_directory_absolute_path}/output')
         for i in range(0, number_of_slices):
             os.remove(f"song_slice_{i}.mp3")
         print("song_without_vocals_and_base.mp3 was successfully created.")
 
 
-def spleet_all_songs_from_directory(dir_path):
-    Path(dir_path + '/SpleetedSongs').mkdir(parents=True, exist_ok=True)
-    for filename in os.listdir(dir_path):
-        spleet_song(
-            dir_path + filename,
-            dir_path + '/SpleetedSongs/' + filename)
+def spleet_and_replace_all_songs_from_directory(working_directory_absolute_path, number_of_stems=2):
+    working_directory_absolute_path = working_directory_absolute_path + '/'
+    for filename in os.listdir(working_directory_absolute_path):
+        if not os.path.isdir(working_directory_absolute_path + filename):
+            spleet_song(
+                working_directory_absolute_path=working_directory_absolute_path,
+                input_file_name=filename,
+                output_file_name=filename,
+                number_of_stems=number_of_stems
+            )
+
+
+# spleet_all_songs_from_directory(
+#     working_directory_absolute_path="/home/mateus/hdd/pastas/git/moises_final/scalableDECIBEL/decibel/utils/dir/",
+#     number_of_stems=2)
+
+# spleet_song(
+#     working_directory_absolute_path="/home/mateus/hdd/pastas/git/moises_final/scalableDECIBEL/decibel/utils/dir",
+#     input_file_name="2.mp3",
+#     output_file_name="output.mp3",
+#     number_of_stems=2)
